2c15fb842b66ce9872e2231808bba08137b26272
kenoma <bghati@yandex.ru>
2022-08-30 22:12:52 +0300

    unlfs

--- KerasFuzzy/examples/case0_debug.py
+++ KerasFuzzy/examples/case0_debug.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, '../layers')
 from ast import Assert
 from fuzzy_layer import FuzzyLayer
+from fuzzy_layer_indep import FuzzyLayerIndep
 from keras.models import Sequential
 import numpy as np
 import tensorflow as tf
@@ -41,3 +42,35 @@ assert np.argmax(fuzzy_layer([1.0, 0.0])) == 2
 # %%
 assert np.argmax(fuzzy_layer([1.0, 1.0])) == 3
 # %%
+
+x = tf.random.uniform((500, 2))
+r = tf.ones([2])
+
+xx = x * (1.0 - x) * r
+# %%
+from logistic_map_layer import LogisticMapLayer
+
+logmap_layer = LogisticMapLayer()
+
+y = logmap_layer(x)
+# %%
+
+import sys
+sys.path.insert(0, '../layers')
+from ast import Assert
+from fuzzy_layer_indep import FuzzyLayerIndep
+from keras.models import Sequential
+import numpy as np
+import tensorflow as tf
+
+x = tf.Variable([[1.0, 1.0],
+                 [2.0, 2.0],
+                 [3.0, 3.0]])
+
+
+min_max = [1, 3]
+
+
+findep = FuzzyLayerIndep(4, min_max)
+y = findep(x)
+y
